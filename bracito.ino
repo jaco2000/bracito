2a541922f7ab316661b1d5d7bce1fc9ec30921a8
jaco2000 <juliana.criolloo@uqvirtual.edu.co>
2019-07-01 16:43:47 -0500

    Trabajo en 3 laboratorio(muestras de proceso)

--- bracito.ino
+++ bracito.ino
@@ -1,4 +1,4 @@
-#include <Servo.h> 
+/*#include <Servo.h> 
 void abrirCerrarMano (void); 
 
 // Defino los pines
@@ -38,10 +38,11 @@ void setup()
   delay (1000);
   motorBase.write (0); //La base queda hacia la derecha
   delay (2000); //Esperar dos segundos para que el brazo vaya completamente hacia atrás 
-  motorDerecho.write (0);
+  motorDerecho.write (20);
   motorIzquierdo.write (90);
   delay (1000); 
   motorDerecho.write (40);
+  motorIzquierdo.write (110);
   delay (1000);
   motorDerecho.write (80);
   delay (1000); //
@@ -55,4 +56,57 @@ void loop(){
   delay (2000);
   motorMano.write (10); //Cerrar Mano
   delay (2000); // Tiempo para repetir el proceso
+}*/
+#include <Servo.h> //Comienzo de trabajo en comandos 
+ 
+ #define pinBase 12
+ #define pinDerecho 11
+ #define pinIzquierdo 10
+ #define pinMano 9
+ 
+ Servo motorBase;
+ Servo motorDerecho;
+ Servo motorIzquierdo;
+ Servo motorMano;
+
+ void setup ()
+ {
+  Serial.begin (9600);
+  motorBase.attach (pinBase);
+  motorDerecho.attach (pinDerecho);
+  motorIzquierdo.attach (pinIzquierdo);
+  motorMano.attach (pinMano);
+ }
+ int m;
+ int Base, Mano, Derecho, Izquierdo;
+ char s[4];
+ 
+void loop (){
+  if (Serial.available ()>0&& Serial.available ()<4)
+  {
+    Serial.readBytes (s,4);
+    Serial.println (s);
+  }
+  
+   switch (s[0])
+  {
+   case 'b':
+   pinBase;
+   break; 
+   case 'g':
+   pinMano;
+   break;
+   case 'd':
+   pinDerecho;
+   break;
+   case 'i':
+   pinIzquierdo;
+   break;
+  }
+  motorBase.write (pinBase);
+  motorDerecho.write (pinDerecho);
+  motorIzquierdo.write (pinIzquierdo);
+  motorMano.write (pinMano);
+  m=((s[1]*100)+(s[2]*10)+(s[3]));
+  
 }
